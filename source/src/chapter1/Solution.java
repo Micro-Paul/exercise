32c62e3ded3a1489ec4a4479d471ef571586b750
微保禄 <455870098@qq.com>
2021-11-19 15:56:55 +0800

    二分查找应用，查找第一个错误的版本

--- source/src/chapter1/Solution.java
+++ source/src/chapter1/Solution.java
@@ -16,7 +16,7 @@ package chapter1;
  * 输出: -1
  * 解释: 2 不存在 nums 中因此返回 -1
  */
-public class Solution {
+public class Solution extends VersionControl {
 
     public static void main(String[] args) {
         Solution test = new Solution();
@@ -38,5 +38,18 @@ public class Solution {
             }
         }
         return -1;
+    }
+
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                return left;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
---
+++ source/src/chapter1/VersionControl.java
@@ -0,0 +1,9 @@
+package chapter1;
+
+public class VersionControl {
+
+    public boolean isBadVersion(int n) {
+        //相关逻辑判断是否试错误版本
+        return true;
+    }
+}
