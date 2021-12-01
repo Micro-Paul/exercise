6711a15550fbf556b279ed4b9b37e97c89f055d5
微保禄 <455870098@qq.com>
2021-12-01 16:50:45 +0800

    有序数组的平方

--- source/src/chapter1/Solution.java
+++ source/src/chapter1/Solution.java
@@ -13,7 +13,7 @@ public class Solution extends VersionControl {
         int[] nums = {-1, 0, 3, 5, 9, 12};
         System.out.println(test.search(nums, 9));
 
-        int[] numss = {1,3,5,6};
+        int[] numss = {1, 3, 5, 6};
         System.out.println(test.searchInsert(numss, 5));
     }
 
@@ -47,15 +47,15 @@ public class Solution extends VersionControl {
     }
 
     public int searchInsert(int[] nums, int target) {
-        int left = 0, right = nums.length -1;
+        int left = 0, right = nums.length - 1;
         while (left <= right) {
-            int mid = left + (right -left) / 2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < target){
+            } else if (nums[mid] < target) {
                 left = mid + 1;
             } else {
-                right = right -1;
+                right = right - 1;
             }
         }
         return left;
@@ -63,7 +63,7 @@ public class Solution extends VersionControl {
 
     /**
      * 给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
-     *
+     * <p>
      * 输入：nums = [-4,-1,0,3,10]
      * 输出：[0,1,9,16,100]
      * 解释：平方后，数组变为 [16,1,0,9,100]
@@ -81,19 +81,58 @@ public class Solution extends VersionControl {
         return result;
     }
 
-//    public int[] sortedSquares2(int[] nums) {
-//        int n = nums.length;
-//        int neg = -1;
-//        for (int i = 0; i < n; ++i) {
-//            if (nums[i] < 0) {
-//                neg = i;
-//            }else {
-//                break;
-//            }
-//        }
-//        int[] result = new int[n];
-//        int index =0, i = neg, j = neg + 1;
-//        while ()
-//
-//    }
+    public int[] sortedSquares2(int[] nums) {
+        int n = nums.length;
+        int neg = -1;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] < 0) {
+                neg = i;
+            } else {
+                break;
+            }
+        }
+        int[] result = new int[n];
+        int index = 0, i = neg, j = neg + 1;
+        while (i >= 0 || j < n) {
+            if (i < 0) {
+                // 全正数情况
+                result[index] = nums[j] * nums[j];
+                ++j;
+            } else if (j == n) {
+                // 全负数情况
+                result[index] = nums[i] * nums[i];
+                --i;
+            } else if (nums[i] * nums[i] < nums[j] * nums[j]) {
+                result[index] = nums[i] * nums[i];
+                --i;
+            } else {
+                result[index] = nums[j] * nums[j];
+                ++j;
+            }
+            ++index;
+        }
+        return result;
+    }
+
+    /**
+     * [-4,-1,0,3,10]
+     * [-7,-3,2,3,11]
+     * @param nums
+     * @return
+     */
+    public int[] sortedSquares3(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        for (int i = 0, j = n - 1, pos = n - 1; i <= j; ) {
+            if (nums[i] * nums[i] > nums[j] * nums[j]) {
+                result[pos] = nums[i] * nums[i];
+                ++i;
+            } else {
+                result[pos] = nums[j] * nums[j];
+                --j;
+            }
+            --pos;
+        }
+        return result;
+    }
 }
